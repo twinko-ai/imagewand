d9f7fc601b2465d2eec60383361aaa9a52a390f0
tengfei <yintengfei@gmail.com>
2025-04-08 10:42:56 -0400

    add coverage

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -17,13 +17,6 @@ def test_cli_version():
     assert "version" in result.output.lower()
 
 
-def test_cli_autofix():
-    runner = CliRunner()
-    result = runner.invoke(cli, ["autofix", "--help"])
-    assert result.exit_code == 0
-    assert "Usage:" in result.output
-
-
 def test_cli_filter():
     runner = CliRunner()
     result = runner.invoke(cli, ["filter", "--help"])
